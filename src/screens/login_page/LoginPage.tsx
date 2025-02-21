088cdfb97f375c542751358f3d32066fddd7f1a0
Gilberto <gil79430@gmail.com>
2025-02-21 15:01:36 -0300

    cria a lógica de capturar dados dos inputs da tela de registro e login

--- src/screens/login_page/LoginPage.tsx
+++ src/screens/login_page/LoginPage.tsx
@@ -1,13 +1,31 @@
 import {Link} from "react-router-dom"
+import { useState } from "react"
+
 
 function LoginPage() {
+    const [userName, setUserName] = useState("")
+    const [userPassword, setUserPassword] = useState("")
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        const user = {
+            name: userName,
+            password: userPassword
+        }
+        
+        console.log(user)
+
+        setUserName("")
+        setUserPassword("")
+    }
+
     return (
         <section className="h-[100vh] flex justify-center items-center">
             <div className="container">
                 <h1 className="text-center h-[15%] leading-[33px]">Login</h1>
-                <form className="h-[85%] flex flex-col items-center justify-evenly" action="">
-                    <input className="input" type="text" placeholder="Usuário" />
-                    <input className="input" type="password" placeholder="Senha" />
+                <form onSubmit={handleSubmit} className="h-[85%] flex flex-col items-center justify-evenly" action="">
+                    <input className="input" type="text" placeholder="Usuário" value={userName} onChange={(e) => {setUserName(e.target.value)}}/>
+                    <input className="input" type="password" placeholder="Senha" value={userPassword} onChange={(e) => {setUserPassword(e.target.value)}}/>
                     <input className="input hover:bg-[var(--button-hover)]" type="submit" value="Entrar" />
                 </form>
                 <p className="text-center">Novo por aqui ? <Link to="/register">Registre-se</Link></p>
--- src/screens/registration_page/RegistrationPage.tsx
+++ src/screens/registration_page/RegistrationPage.tsx
@@ -1,13 +1,34 @@
+import { useState } from "react"
+
 function RegistrationPage() {
+    const [userCreateName, setCreateName] = useState("")
+    const [userCreatePassword, setCreatePassword] = useState("")
+    const [userRepeatPassword, setRepeatPassword] = useState("")
+
+    function handleRegister(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        const rgtrUser = {
+            name: userCreateName,
+            password: userCreatePassword
+        }
+
+        console.log(rgtrUser)
+
+        setCreateName("")
+        setCreatePassword("")
+        setRepeatPassword("")
+    }
+
     return (
         <section className="h-[100vh] flex justify-center items-center">
             <div className="container">
                 <h1 className="h-[15%] text-center">Bem vindo! Complete aqui o seu cadastro</h1>
-                <form className="h-[85%] flex flex-col items-center justify-between" action="">
-                    <input className="input" type="text" placeholder="Nome"/>
-                    <input className="input" type="password" placeholder="Senha" />
-                    <input className="input" type="password" placeholder="Repetir senha" />
-                    <input className="input hover:bg-[var(--button-hover)]  " type="submit" value="Registrar" />
+                <form onSubmit={handleRegister} className="h-[85%] flex flex-col items-center justify-between" action="">
+                    <input className="input" type="text" placeholder="Nome" value={userCreateName} onChange={(e) => {setCreateName(e.target.value)}}/>
+                    <input className="input" type="password" placeholder="Senha" value={userCreatePassword} onChange={(e) => {setCreatePassword(e.target.value)}}/>
+                    <input className="input" type="password" placeholder="Repetir senha" value={userRepeatPassword} onChange={(e) => {setRepeatPassword(e.target.value)}}/>
+                    <input className="input hover:bg-[var(--button-hover)]  " type="submit" value="Registrar"/>
                 </form>
             </div>
         </section>
