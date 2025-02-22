34efc4f1cc18d9f512dd2489066bb7117183edcf
Gilberto <gil79430@gmail.com>
2025-02-22 10:15:49 -0300

    feat: captura a entrada do usuário e insere no componente de nota

--- src/screens/application_page/components/Main.tsx
+++ src/screens/application_page/components/Main.tsx
@@ -1,13 +1,36 @@
 import NoteContainer from "./main_components/NoteContainer"
+import { useState } from "react"
+
 function Main() {
+    const [noteTitle, setNoteTitle] = useState("")
+    const [noteDescription, setNoteDescription] = useState("")
+    const [note, setNote] = useState({
+        title: "",
+        description: ""
+    })
+    
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        const newNote = {
+            title: noteTitle,
+            description: noteDescription
+        }
+
+        setNoteTitle("")
+        setNoteDescription("")
+        setNote(newNote)
+    }
+    
     return (
-        <main className="flex flex-col items-center gap-[25px] ">
-            <form action="" className="h-[136px] bg-[var(--bg-color-container)] w-[335px] h-[58px] flex items-center p-[15px] rounded-[10px] shadow-[0px_5px_10px_var(--box-shadow)] flex-col justify-between">
-                <input className="bg-[var(--bg-color-primary)] w-full p-[5px] rounded-[5px]" type="text" placeholder="Título da nota"/>
-                <input className="bg-[var(--bg-color-primary)] w-full p-[5px] rounded-[5px]" type="text" placeholder="Descrição da nota"/>
-                <input className="bg-[var(--bg-color-primary)] w-full p-[5px] rounded-[5px] hover:bg-[var(--button-hover)]" type="button" value="Adicionar"/>
+        <main className="flex flex-col items-center gap-[25px]">
+            <form action="" onSubmit={handleSubmit} className="h-[136px] bg-[var(--bg-color-container)] w-[335px] h-[58px] flex items-center p-[15px] rounded-[10px] shadow-[0px_5px_10px_var(--box-shadow)] flex-col justify-between">
+                <input value={noteTitle} onChange={(e) => {setNoteTitle(e.target.value)}} className="bg-[var(--bg-color-primary)] w-full p-[5px] rounded-[5px]" type="text" placeholder="Título da nota"/>
+                <input value={noteDescription} onChange={(e) => {setNoteDescription(e.target.value)}} className="bg-[var(--bg-color-primary)] w-full p-[5px] rounded-[5px]" type="text" placeholder="Descrição da nota"/>
+                <input className="bg-[var(--bg-color-primary)] w-full p-[5px] rounded-[5px] hover:bg-[var(--button-hover)]" type="submit" value="Adicionar"/>
             </form>
-            <NoteContainer/>
+            <NoteContainer note={note}/>
         </main>
     )
 }
--- src/screens/application_page/components/main_components/NoteContainer.tsx
+++ src/screens/application_page/components/main_components/NoteContainer.tsx
@@ -1,8 +1,23 @@
-function NoteContainer () {
+interface Note {
+    title: string,
+    description: string
+}
+
+type noteType = {
+    note: Note
+}
+
+function NoteContainer (noteProp: noteType) {
+
+    const note = {
+        title: noteProp.note.title,
+        description: noteProp.note.description
+    }
+
     return(
         <div className="w-[335px] bg-[var(--bg-color-container)] flex gap-[13px] flex-col p-[13px_15px] shadow-[0px_5px_10px_var(--box-shadow)] rounded-[10px]">
-            <h2 className="bg-[var(--bg-color-primary)] h-[63px] text-[20px] pl-[10px] leading-[63px] rounded-[5px]">Título</h2>
-            <p className="bg-[var(--bg-color-primary)] h-[110px] pl-[10px] rounded-[5px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, dolorum?</p>
+            <h2 className="bg-[var(--bg-color-primary)] h-[63px] text-[20px] pl-[10px] leading-[63px] rounded-[5px] overflow-clip">{note.title}</h2>
+            <p className="bg-[var(--bg-color-primary)] h-[110px] pl-[10px] rounded-[5px] overflow-scroll">{note.description}</p>
             <div className="flex justify-between">
                 <div className="flex items-center gap-[20px]">
                     <button><img className="w-[25px]" src="/trash3.svg" alt="trash icon" /></button>
@@ -10,6 +25,17 @@ function NoteContainer () {
                 </div>
                 <button><img className="w-[25px]" src="/check2.svg" alt="check icon" /></button>
             </div>
+            {/*<div className="w-[335px] bg-[var(--bg-color-container)] flex gap-[13px] flex-col p-[13px_15px] shadow-[0px_5px_10px_var(--box-shadow)] rounded-[10px]">
+                <h2 className="bg-[var(--bg-color-primary)] h-[63px] text-[20px] pl-[10px] leading-[63px] rounded-[5px]">Título</h2>
+                <p className="bg-[var(--bg-color-primary)] h-[110px] pl-[10px] rounded-[5px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, dolorum?</p>
+                <div className="flex justify-between">
+                    <div className="flex items-center gap-[20px]">
+                        <button><img className="w-[25px]" src="/trash3.svg" alt="trash icon" /></button>
+                        <button><img className="w-[25px]" src="/pencil-square.svg" alt="edit icon" /></button>
+                    </div>
+                    <button><img className="w-[25px]" src="/check2.svg" alt="check icon" /></button>
+                </div>
+            </div>*/}
         </div>
     )
 }
